4ebd8004fd4c16c1123c7d00251051ee37044e18
mobeen.ahmad <mobeen.ahmad@innotech-sa.com>
2025-10-19 12:48:27 +0500

    Checking in All the changes after adding DB connection

---
+++ B2B/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace B2B.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+
+        // Example table
+        public DbSet<User> Users { get; set; }
+    }
+}

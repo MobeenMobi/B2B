ef9c440da105d2a89d8337ac5370f53ed16d4b6e
shiek <shiek@MOBEEN-PC>
2025-12-06 15:33:32 +0500

    Checking in all the code

--- B2B/Models/Users.cs
+++ B2B/Models/Users.cs
@@ -10,17 +10,9 @@ namespace B2B.Models
         public string FirstName { get; set; }
         public string? MiddleName { get; set; }
         public string? LastName { get; set; }
-        public string Nationality { get; set; }
-        public DateTime DateOfBirth { get; set; }
-        public string Gender { get; set; }
         public string IDType { get; set; }
         public string IDNumber { get; set; }
         public string PhoneNumber { get; set; }
-        public string Address { get; set; }
-        public string Country { get; set; }
-        public string City { get; set; }
-        public string Postcode { get; set; }
-        public string State { get; set; }
         public string Occupation { get; set; }
         public string? ApproverName { get; set; }
         public bool Agree { get; set; }
